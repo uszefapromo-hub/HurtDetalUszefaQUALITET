30b4ff1c4e025552721bf980fc23b73858ffc6e5
TwojeImie <qualitetuszefapromo@gmail.com>
2026-03-18 17:55:33 +0100

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("start backend");
+git add .
+git commit -m "init"
+git remote add origin https://github.com/uszefapromo-hub/HurtDetalUszefaQUALITET.git
+git branch -M main
+git push -u origin main
+
