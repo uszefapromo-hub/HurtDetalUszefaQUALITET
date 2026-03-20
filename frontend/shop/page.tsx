4bbdd3c871e3098f0f4dfcfe75a4e4d2b974ed96
Henryk Slusarski <qualitetuszefapromo@gmail.com>
2026-03-20 10:30:10 +0100

    Add ShopPage component with iframe for sklep.htm

---
+++ frontend/shop/page.tsx
@@ -0,0 +1,12 @@
+export default function ShopPage() {
+  return (
+    <iframe
+      src="/sklep.htm"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none"
+      }}
+    />
+  );
+}
